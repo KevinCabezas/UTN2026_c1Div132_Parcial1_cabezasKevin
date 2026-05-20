bf86255d023573f3c70582b0f49a08a925c529da
KevinCabezas <angelescabesuchi@gmail.com>
2026-05-20 11:26:44 -0300

    Parcial terminado

--- js/index.js
+++ js/index.js
@@ -1,33 +1,113 @@
 //--- Funcion que obtiene el carrito del LocalStorage, lo parsea a un array y lo retorna ---//
-function obtenerCarrito() 
-{
-    return;
+function obtenerCarrito() {
+  let carrito = localStorage.getItem("carrito");
+
+  if (carrito === null) {
+    return [];
+  }
+  return JSON.parse(carrito);
 }
 
 //--- Funcion que guarda el carrito recibido al LocalStorage, previamente transformado a string ---//
-function guardarCarrito(carrito) 
-{
-
+function guardarCarrito(carrito) {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function sumarAlCarrito(e) 
-{
-    //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
-    let elementoClickeado = e.target;
+// -----------------------------------------------------------------------------------------------------------------
+// 1-Captura el elemento que disparo el evento
+// 2-Navega en el DOM hasta el <li> que corresponde al producto
+// 3-Obtiene el nombre y precio del producto desde el DOM
+// 4-Recupera la lista actual de productos guardados en el carrito desde localstorage
+// 5-Busca dentro de la lista si el producto ya fue agregado utilizando find()
+// 6-Si el producto ya existe en la lista se incrementa en 1 su cantidad
+// 7-Si el producto no existe se agrega a la lista con cantidad inicial en 1
+// 8-Actualiza la lista con los cambios realizados
+// 9-Guarda la lista actualizada nuevamente en localstorage
+// 10-Muestra un mensaje al usuario confirmando que el producto fue agregado 
+
+function sumarAlCarrito(e) {
+  //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
+  let elementoClickeado = e.target;
+
+  let li = elementoClickeado.parentElement;
+
+  let nombre = li.querySelector(".nombre-producto").textContent;
+  let precio = li.querySelector(".precio-producto").textContent;
+
+  let carrito = obtenerCarrito();
+
+  let productoExistente = carrito.find(producto => producto.nombre === nombre);
+
+  if (productoExistente) {
+    productoExistente.cantidad += 1;
+  }
+  else {
+    carrito.push({
+      nombre: nombre,
+      precio: precio,
+      cantidad: 1
+    });
+  }
+
+  console.log(carrito);
+  guardarCarrito(carrito);
+
+  alert(`${nombre} se agrego al carrito`);
 }
 
-function restarDelCarrito(e) 
-{
-    //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
-    let elementoClickeado = e.target;
+
+// ------------------------------------------------------------------------------------------------------------------------
+// 1-Captura el elemento que disparo el evento
+// 2-Navega en el DOM hasta el <li> que corresponde al producto
+// 3-Obtiene el nombre del producto desde el DOM.
+// 4-Recupera el estado actual del carrito desde localstorage
+// 5-Si el carrito esta vacio se interrumpe la ejecucion
+// 6-Busca el producto en la lista utilizando find()
+// 7-Si no existe se informa al usuario y corta la ejecucion
+// 8-Si existe se resta en 1 a cantidad
+// 9-Si la cantidad llega a 0 se elimina el producto utilizando filter()
+// 10-Guarda el nuevo estado actualizado en localstorage
+
+function restarDelCarrito(e) {
+  //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
+  let elementoClickeado = e.target;
+
+  let li = elementoClickeado.parentElement;
+
+  let nombre = li.querySelector(".nombre-producto").textContent;
+
+  let carrito = obtenerCarrito();
+
+  if (carrito.length === 0) {
+    alert("No hay ningún producto guardado en el carrito");
+    return;
+  }
+
+  let productoExistente = carrito.find(producto => producto.nombre === nombre);
+
+  if (!productoExistente) {
+    alert(`No hay mas ${nombre} en el carrito`);
+    return;
+  }
+
+  productoExistente.cantidad -= 1;
+
+  alert(`${nombre} se elimino del carrito`);
+
+  if (productoExistente.cantidad === 0) {
+    carrito = carrito.filter(producto => producto.nombre !== nombre);
+  }
+
+  console.log(carrito);
+
+  guardarCarrito(carrito);
 }
 
 //--- [EVENTOS] Asociacion del evento "click" a los botones "+" y "-" con la funcion manejadora del evento ---//
-window.addEventListener("DOMContentLoaded", () => 
-{
-    const botonesSumar = document.querySelectorAll(".btn-sumar-a-carrito");
-    const botonesRestar = document.querySelectorAll(".btn-restar-a-carrito");
+window.addEventListener("DOMContentLoaded", () => {
+  const botonesSumar = document.querySelectorAll(".btn-sumar-a-carrito");
+  const botonesRestar = document.querySelectorAll(".btn-restar-a-carrito");
 
-    botonesSumar.forEach(btn => btn.addEventListener("click", sumarAlCarrito));
-    botonesRestar.forEach(btn => btn.addEventListener("click", restarDelCarrito));
+  botonesSumar.forEach(btn => btn.addEventListener("click", sumarAlCarrito));
+  botonesRestar.forEach(btn => btn.addEventListener("click", restarDelCarrito));
 });
--- pages/carrito/carrito.js
+++ pages/carrito/carrito.js
@@ -1,21 +1,85 @@
-function obtenerCarrito() 
-{
+function obtenerCarrito() {
+  let carritoGuardado = localStorage.getItem("carrito");
+
+  if (carritoGuardado === null) {
+    return [];
+  }
+  return JSON.parse(carritoGuardado);
+}
+
+// --------------------------------------------------------------------------------------------------
+
+// 1-Obtiene la referencia de la tabla donde se mostaran los productos
+// 2-Recupera la lista actual del carrito desde localstorage
+// 3-Inicializa una variable para acumular el total final a pagar
+// 4-Si la lista esta vacia muestra $0 como total y detiene la ejecucion
+// 5-Recorre cada producto de la lista
+// 6-Verifica que la cantidad del producto sea mayor o igual a 1
+// 7-Parsea el precio  en un numero
+// 8-Calcula el subtotal multiplicando precio por cantidad
+// 9-Acumula el subtotal en el total final
+// 10-Crea una fila de la tabla con los datos del producto usando innerHTML
+// 11-Muestra el total final actalizado haciendo cantidad por precio
+function cargarProductosCarrito() {
+  let tabla = document.getElementById("tabla-carrito");
+  let carrito = obtenerCarrito();
+
+  let totalFinal = 0;
+
+  if (carrito.length === 0) {
+    document.getElementById("valor-final").textContent =
+      "El valor final a pagar es: $0";
     return;
+  }
+
+  carrito.forEach(producto => {
+    
+    if (producto.cantidad >= 1) {
+      let precioNumero = parseInt(producto.precio.replace("$", ""));
+      let subtotal = precioNumero * producto.cantidad;
+
+      totalFinal += subtotal;
+
+      let filaHTML = `
+        <tr>
+          <td>${producto.nombre}</td>
+          <td>${producto.cantidad}</td>
+          <td>${producto.precio}</td>
+        </tr>`;
+
+      tabla.innerHTML += filaHTML;
+    }
+  });
+
+  document.getElementById("valor-final").textContent =
+    `El valor final a pagar es: $${totalFinal}`;
 }
 
-function cargarProductosCarrito() 
-{
-    let tabla = document.getElementById("tabla-carrito");
-}
+// ------------------------------------------------------------------------------------------------------
+// 1-Elimina la lista de productos guardada en localstorage
+// 2-Muestra un mensaje alert confirmando que el carrito fue limpiado
+// 3-Restablece la tabla dejando solo la fila de encabezado
+// 4-Actualiza el total final mostrando $0
 
-function limpiarCarrito() 
-{
+function limpiarCarrito() {
+  localStorage.removeItem("carrito");
 
+  alert("Carrito limpiado");
+
+  document.getElementById("tabla-carrito").innerHTML = `
+    <tr class="fila-header-carrito">
+      <td class="celda-header-tabla-carrito">Nombre del producto</td>
+      <td class="celda-header-tabla-carrito">Cantidad</td>
+      <td class="celda-header-tabla-carrito">Precio unitario</td>
+    </tr>`;
+
+  document.getElementById("valor-final").textContent =
+    "El valor final a pagar es: $0";
 }
 
 // Asociar evento al botón cuando la página carga
-window.addEventListener("DOMContentLoaded", () =>
-{
-    cargarProductosCarrito();
-    document.querySelector(".btn-limpiar-carrito").addEventListener("click", limpiarCarrito);
+window.addEventListener("DOMContentLoaded", () => {
+  cargarProductosCarrito();
+  document.querySelector(".btn-limpiar-carrito").addEventListener("click", limpiarCarrito);
 });
+
